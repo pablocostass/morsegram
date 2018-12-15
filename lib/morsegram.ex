ddeb6f5b8aea9949235e3833ce8ed30b4e9cd4ba
Javier Garea <javier.garea@udc.es>
2018-12-15 16:32:11 +0100

    Client interface implementation

--- lib/morsegram.ex
+++ lib/morsegram.ex
@@ -12,12 +12,69 @@ defmodule Morsegram do
       :world
 
   """
-  def hello do
-    :world
+  def main(argv) do
+    argv
+    |> parse_args()
+    |> set_parameters_up()
+    |> prompt()
   end
 
-  def help do
-    IO.puts("Morsegram.hello()")
+  defp parse_args(args) do
+    {parsed, _rest, _invalid} = 
+      OptionParser.parse(args, strict: [user: :string, server: :string])
+    [user: usr, server: srvr] = parsed
+    {usr, srvr}
   end
 
+  defp set_parameters_up({user, server}) do
+    String.to_atom(user)
+    |> Node.start()
+    String.to_atom(server)
+    |> Node.connect()
+    IO.puts("Welcome to Morsegram!")
+    user
+  end  
+
+  defp prompt(username) do
+    {:ok, [cmd, arg]} = :io.fread('$ ', '~ts ~ts')
+    run(username, cmd, arg)
+    prompt(username)
+  end
+
+  defp run(username, 'search_room', room) do
+    Client.search_room(room, username)
+  end
+  defp run(username,'send_message', room) do
+    msg = :io.get_line('#{username}: ')
+    Client.send_message(msg, room, username)
+  end
+  defp run(username, 'list_users', room) do
+    Client.list_users(room, username)
+  end
+  defp run(username, 'disconnect', room) do
+    Client.disconnect_from(room, username)
+  end
+  defp run(username, '#', 'quit') do
+    IO.puts "Disconnecting from Morsegram..."
+    IO.puts "See you later, #{username}"
+    System.halt(0)
+  end
+  defp run(_, '#', 'help') do
+    IO.puts "Commands:"
+    IO.puts "search_room [room]"
+    IO.puts "\t Finds a given room and connects you to it"
+    IO.puts "send_message [room]"
+    IO.puts "\t Gives you a prompt where you can type a message that will be sent to the given room"
+    IO.puts "list_users [room]"
+    IO.puts "\t Shows the list of users in a given room"
+    IO.puts "disconnect [room]"
+    IO.puts "\t Disconnects you from a given room"
+    IO.puts "# help"
+    IO.puts "\t Prints out this message"
+    IO.puts "# quit"
+    IO.puts "\t Closes Morsegram after printing you a goodbye message"
+  end
+  defp run(_, cmd, arg) do
+    IO.puts "Unknown command '#{cmd} #{arg}', run the command '#help' to get the list of possible commands!"
+  end
 end
